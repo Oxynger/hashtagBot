92301dccb40fef6a934aaff056eaaed2ed1833ee
oxynger <oxynger@bk.ru>
2019-12-14 01:58:58 +0000

    add proxy support

--- main.js
+++ main.js
@@ -3,17 +3,24 @@ import proxy from "https-proxy-agent"
 import { generate } from "./hastag_generator.js"
 
 const token = process.env.BOT_TOKEN
-const proxy_url = process.env.PROXY_URL
+const telegrapfOption = proxyOptions(process.env.PROXY_URL)
+
+function proxyOptions(proxy_url) {
+    if (proxy_url) {
+        return {
+            telegram: {
+                agent: proxy(proxy_url)
+            }
+        }
+    }
+    return {}
+}
 
 function greeter(name) {
     return generate(`Hello ${name}!`)
 }
 
-let bot = new telegraf(token, {
-    telegram: {
-        agent: proxy(proxy_url)
-    }
-})
+let bot = new telegraf(token, telegrapfOption)
 
 bot.start((ctx) => {
     userName = ctx.message.from.first_name
