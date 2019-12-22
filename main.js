99e48569e89078a62339f93b0c699a9a7439aaef
oxynger <oxyner@gmail.com>
2019-12-22 15:54:51 +0400

    add markdown decarator

--- main.js
+++ main.js
@@ -17,19 +17,28 @@ function proxyOptions(proxy_url) {
     return {}
 }
 
+function markdownDecarator(func) {
+    return function () {
+        return `\`${func.apply(this, arguments)}\``;
+    }
+}
+
 function greeter(name) {
     return generate(`Hello ${name}!`);
 }
+
+greeter = markdownDecarator(greeter);
+let generateMarkdownMsg = markdownDecarator(generate);
 
 const bot = new telegraf(TOKEN, telegrapfOption);
 
 bot.start((ctx) => {
     let userName = ctx.message.from.first_name;
-    return ctx.reply(greeter(userName));
+    return ctx.replyWithMarkdown(greeter(userName));
 })
 
 bot.on('text', (ctx) => {
-    try { return ctx.reply(generate(ctx.message.text)) }
+    try { return ctx.replyWithMarkdown(generateMarkdownMsg(ctx.message.text)) }
     catch (e) {
         console.log(e.stack);
         return reply(e.message);
