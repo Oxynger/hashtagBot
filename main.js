8f849fbc17186b3a0f437280f430a44060a0fa57
oxynger <oxyner@gmail.com>
2019-12-22 15:01:14 +0400

    remove express from dependency

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 import telegraf from "telegraf";
 import proxy from "https-proxy-agent";
-import express from "express";
 import { generate } from "./hastag_generator.js";
 
 const TOKEN = process.env.BOT_TOKEN;
-const PORT = process.env.PORT || 3000;
-const URL = process.env.URL;
 
 const telegrapfOption = proxyOptions(process.env.PROXY_URL);
 
@@ -24,8 +21,6 @@ function greeter(name) {
     return generate(`Hello ${name}!`);
 }
 
-const expressApp = new express();
-
 const bot = new telegraf(TOKEN, telegrapfOption);
 
 bot.start((ctx) => {
@@ -33,19 +28,11 @@ bot.start((ctx) => {
     return ctx.reply(greeter(userName));
 })
 
-bot.on('text', ({ message: { text }, reply }) => {
-    try { return reply(generate(text)) }
+bot.on('text', (ctx) => {
+    try { return ctx.reply(generate(ctx.message.text)) }
     catch (e) {
         console.log(e.stack);
         return reply(e.message);
     }
 })
 bot.launch();
-
-expressApp.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-expressApp.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
